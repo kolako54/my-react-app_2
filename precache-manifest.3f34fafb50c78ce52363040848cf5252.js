2aa124a0d2cb9a57dbcff0747ca7880fca256e9b
kolako54 <parhamwolf051@gmail.com>
2020-04-26 18:10:14 +0430

    Updates

---
+++ precache-manifest.3f34fafb50c78ce52363040848cf5252.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64ac7a1b1dbbdf1627c98eb48daa170",
+    "url": "/my-react-app_2/index.html"
+  },
+  {
+    "revision": "2108764868b7061d5791",
+    "url": "/my-react-app_2/static/css/main.a5223025.chunk.css"
+  },
+  {
+    "revision": "1e724dee6acd5dba9826",
+    "url": "/my-react-app_2/static/js/2.b3e37497.chunk.js"
+  },
+  {
+    "revision": "2108764868b7061d5791",
+    "url": "/my-react-app_2/static/js/main.4ceb764f.chunk.js"
+  },
+  {
+    "revision": "bd8e036d8b262d4229c4",
+    "url": "/my-react-app_2/static/js/runtime-main.ca38f194.js"
+  },
+  {
+    "revision": "358cf17eb92a8747b28e0a0e9b797317",
+    "url": "/my-react-app_2/static/media/Logo.358cf17e.svg"
+  },
+  {
+    "revision": "f2bd4a28e444d8f2ac28e9ce07b486b2",
+    "url": "/my-react-app_2/static/media/Vazir-Medium.f2bd4a28.woff"
+  },
+  {
+    "revision": "80b42e97beac2535b0b3acee1d1edaec",
+    "url": "/my-react-app_2/static/media/aiside.80b42e97.svg"
+  }
+]);
